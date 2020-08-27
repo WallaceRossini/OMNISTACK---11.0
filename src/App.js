7e80aab8ff5a56e4e379a578720ed90d72f4d87c
Wallace Rossini <rossiniwallace@gmail.com>
2020-08-26 22:57:13 -0300

    Finalizada página de Login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import Header from './Header';
+
+import './global.css';
+
+import SignIn from './pages/SignIn';
 
 function App() {
   return (
-    <Header title="Be The Hero" />
+    <SignIn />
   );
 }
 
